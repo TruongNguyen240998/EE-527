29f392dd687c6e0556c805a0aec8f33e3b0c9442
Truong Nguyen <71046610+TruongNguyen240998@users.noreply.github.com>
2020-09-09 22:00:45 -0400

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -5,14 +5,16 @@ def predict(row,weights,bk):
     for i in range(len(row)):
             activation += weights[i]*row[i]
     return 1 if activation >=0 else 0
-'q/a'
+
+'Question a'
 dataset = [[0,0],[1,0],[0,1],[1,1]]
 w = [1,1]
 for row in dataset:
     AND=predict(row,w,-2)
     OR=predict(row,w,-1)
     print("Input:{0}   And:{1}      OR:{2}".format(row,AND,OR))
-'q/b'
+
+ 'Question b'
 from itertools import product
 x5 = list(product([0,1],repeat = 5))
 w5=[1,1,1,1,1]
@@ -20,7 +22,8 @@ for row in x5:
     AND=predict(row,w5,-2)
     OR=predict(row,w5,-1)
     print("Input:{0}   And:{1}      OR:{2}".format(row,AND,OR))
-'q/c'
+    
+'Question c'
 print("Number of inputs:")
 n=input()
 from itertools import product
@@ -34,5 +37,5 @@ for row in xn:
 '''
 Comment: according to the result from question b and c, I found out that the AND 
 gate is only true when there is 2 inputs, otherwise it false. In order to work well with n inputs, bk must be equal -n.
-For the OR gate, bias does not need to be changed to be true in any number of input.
+For the OR gate, the bias does not need to be changed to be true in any number of input.
 '''
